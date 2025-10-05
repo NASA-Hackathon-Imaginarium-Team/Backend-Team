ef16ce6415e57bbc50b2fc14a5e6c8bcc881ca8b
Iain Simpson <iainsimpsonstarred@gmail.com>
2025-10-05 11:20:27 +0100

    Added requirement.txt for render

--- server.py
+++ server.py
@@ -4,24 +4,14 @@ import joblib
 import pandas as pd
 
 XGBoostModel = joblib.load("pkl_files/xgb_model.pkl")
-
-model = XGBoostModel
+keplerGBM = joblib.load("pkl_files/light_gbm_model_kepler.pkl")
+tessGBM = joblib.load("pkl_files/TESS_LightGBM.pkl")
 
 app = FastAPI()
 
 @app.get("/")
 def returnHello():
     return {"message": "Hello"}
-
-@app.post("/predict")
-def predict(data: dict):
-    df = pd.DataFrame([data])
-    prediction = model.predict(df)[0]
-    probs = model.predict_proba(df)[0].tolist()
-    return {
-        "prediction": prediction,
-        "probabilities": probs
-    }
 
 class Features(BaseModel):
     data: dict
@@ -31,14 +21,39 @@ def predict(features: Features):
     # Convert to DataFrame
     df = pd.DataFrame([features.data])
     
-    # Make prediction
-    prediction = model.predict(df)[0]
-    probs = model.predict_proba(df)[0].tolist()
-    
+    prediction = keplerGBM.predict(df)[0]
+    probs = keplerGBM.predict_proba(df)[0].tolist()
+
+    predictionText = ""
+    if (prediction == 1):
+        predictionText = "Probable"
+    elif (prediction == 0):
+        predictionText = "Improbable"
+
     return {
-        "prediction": prediction,
+        "prediction": predictionText,
         "probabilities": probs
     }
+
+@app.post("/predict/tess")
+def predict(features: Features):
+    # Convert to DataFrame
+    df = pd.DataFrame([features.data])
+    
+    prediction = tessGBM.predict(df)[0]
+    probs = tessGBM.predict_proba(df)[0].tolist()
+
+    predictionText = ""
+    if (prediction == 1):
+        predictionText = "Probable"
+    elif (prediction == 0):
+        predictionText = "Improbable"
+
+    return {
+        "prediction": predictionText,
+        "probabilities": probs
+    }
+
 
 
 if __name__ == "__main__":
